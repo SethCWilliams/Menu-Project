9edf2434f3f479755b45bc9d0b1a3b6690311833
Seth Williams <swilliams9334@gmail.com>
2019-02-21 16:26:15 -0500

    getting files setup...maybe

--- conf/settings.py
+++ conf/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'whitenoise',
     'psycopg2',
+    'rest_framework',
 
     'orders',
 ]
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MenuThaime',
+        'USER': 'Restaurateur',
+        'PASSWORD': 'safepass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Order(models.Model):
+    # ONE = 1
+    # TWO = 2
+    # THREE = 3
+    # FOUR = 4
+    # FIVE = 5
+    # SIX = 6
+    # SEVEN = 7
+    # EIGHT = 8
+    # NINE = 9
+    # TEN = 10
+    # SPICE_CHOICES = ((ONE, 1), (TWO, 2), (THREE, 3), (FOUR, 4), (FIVE, 5), (SIX, 6), (SEVEN, 7), (EIGHT, 8), (NINE, 9), (TEN, 10))
+    # user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL())
+    item = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=1)
+    description = models.TextField()
+    # spice = models.CharField(
+    #     max_length=2,
+    #     choices=SPICE_CHOICES,
+    #     default=3,
+    # )
